95b5d88ecf9fd4e3b48e0320bd705503ed19741a
lanaeltambouly <lanaeltambouly@gmail.com>
2026-05-07 02:06:01 +0300

    header file

--- include/Bank.h
+++ include/Bank.h
@@ -1,3 +1,38 @@
 #pragma once
+#include "types.h"
+#include "Account.h"
+#include "Customer.h"
+#include <unordered_map>
+#include <vector>
+#include <memory>
 
-class Bank {};
+class Bank {
+    private:
+    TransactionRecord tx_manager;
+    std::unordered_map<uint64_t, std::unique_ptr<Account>> accounts;
+
+    public:
+    Bank();
+    
+    AccountState openAccount(uint64_t customer_id);
+
+    AccountState closeAccount(uint64_t account_id);
+
+    AccountState freezeAccount(uint64_t account_id);
+
+    AccountState unfreezeAccount(uint64_t account_id);
+
+    TransactionStatus deposit(uint64_t account_id, uint64_t amount);
+
+    TransactionStatus withdraw(uint64_t account_id, uint64_t amount);
+
+    TransactionStatus transfer(uint64_t account_id, uint64_t amount);
+
+    int64_t getBalance(uint64_t account_id) const;
+
+    std::vector<TransactionRecord> queryBySrc(uint64_t account_id);
+
+    std::vector<TransactionRecord> queryByDest(uint64_t account_id);
+
+
+};
